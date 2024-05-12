d68fb7f86319c5f12adb69955f424c3124a85958
PerfectionistAF <ihabsohaila@yahoo.com>
2024-05-12 16:08:07 +0300

    Welcome screen trial

--- security_in_color.py
+++ security_in_color.py
@@ -5,12 +5,16 @@ init()
 
 #print(Fore.CYAN + 'This text is red in color')
 
-cprint("\nThis text is red in color\n", "red", attrs=["dark"])
-
+cprint("\n\t****WELCOME TO SECURITY SUITE 1.0****\t\n", "cyan", attrs=["dark"])
+cprint("\n  ____      _____      ____", "light_cyan")
+cprint("\n  \   \    /     \    /   /", "light_cyan")
+cprint("\n   \   \__/   _   \__/   /", "light_cyan")
+cprint("\n    \        / \        /" , "light_cyan")
+cprint("\n     \______/   \______/" , "light_cyan")
 #print(Back.YELLOW + 'The text with Green background')
 #print(Style.BRIGHT + 'The text is DIM now')
 
 
 #print(Fore.MAGENTA + 'This text is red in color')
 
-cprint("This text is red in color\n", "light_cyan")
+cprint("\nThis text is red in color\n", "red")
